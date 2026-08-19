453ab858bbc64f25cb0757439f03b1b5db4ae85a
hmhood1-hash <hmhood1@gmail.com>
2026-08-19 18:12:24 -0400

    Corregir login_view en app.py para usar nombre del blueprint

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'  # ← CORREGIDO: usar nombre del blueprint
 login_manager.login_message = 'Por favor inicia sesión para acceder a esta página.'
 login_manager.login_message_category = 'info'
 
